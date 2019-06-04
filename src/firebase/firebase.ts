c20d6ee9cffad3f86261c4dded53f556800de9b9
Kiichi Tachibana <>
2019-06-04 21:47:10 +0900

    Implement firebase module

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,4 +2,29 @@ import app from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from "./config";
 
-app.initializeApp(firebaseConfig);
+class Firebase {
+  auth: any;
+
+  constructor() {
+    app.initializeApp(firebaseConfig);
+    this.auth = app.auth();
+  }
+
+  async register(name: string, email: string, password: string) {
+    const newUser = await this.auth.createUserWithEmailAndPassword(
+      email,
+      password
+    );
+    return await newUser.user.updateProfile({
+      displayName: name
+    });
+  }
+
+  async login(email: string, password: string) {
+    return await this.auth.signInWithEmailAndPassword(email, password);
+  }
+}
+
+const firebase = new Firebase();
+
+export default firebase;
