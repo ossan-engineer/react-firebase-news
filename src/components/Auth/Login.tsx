f7d1dec029d992147a5bf58169753bc56d99b3f8
Kiichi Tachibana <>
2019-06-04 22:17:16 +0900

    Implement useAuth for tracking user sessions

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -28,6 +28,7 @@ const Login = (props: any) => {
       const response = login
         ? await firebase.login(email, password)
         : await firebase.register(name, email, password);
+      props.history.push("/");
     } catch (err) {
       console.error("Authentication Error", err);
       setFirebaseError(err.message);
