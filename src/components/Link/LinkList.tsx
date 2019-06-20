900c68774ffb0049f74d8839490b13395184b1ec
Kiichi Tachibana <>
2019-06-20 21:42:22 +0900

    Display links

--- src/components/Link/LinkList.tsx
+++ src/components/Link/LinkList.tsx
@@ -1,8 +1,10 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { FirebaseContext } from "../../firebase";
+import LinkItem from "./LinkItem";
 
 function LinkList(props: any) {
   const { firebase } = useContext(FirebaseContext);
+  const [links, setLinks] = useState([]);
 
   useEffect(() => {
     getLinks();
@@ -16,10 +18,21 @@ function LinkList(props: any) {
     const links = snapshot.docs.map(doc => {
       return { id: doc.id, ...doc.data() };
     });
-    console.log({ links });
+    setLinks(links);
   }
 
-  return <div>LinkList</div>;
+  return (
+    <div>
+      {links.map((link: any, index: number) => (
+        <LinkItem
+          key={link.id}
+          showCount={true}
+          link={link}
+          index={index + 1}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default LinkList;
