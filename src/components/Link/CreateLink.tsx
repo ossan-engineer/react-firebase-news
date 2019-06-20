5e4d64c92ed2efe692019398800d042ba8d44d9a
Kiichi Tachibana <>
2019-06-20 20:43:46 +0900

    Add Firestore to create link

--- src/components/Link/CreateLink.tsx
+++ src/components/Link/CreateLink.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import useFormValidation from "../Auth/useFormValidation";
 import validateCreateLink from "../Auth/validateCreateLink";
 import { text } from "body-parser";
+import { FirebaseContext } from "../../firebase";
 const INITIAL_STATE = {
   description: "",
   url: ""
 };
 
 function CreateLink(props: any) {
+  const { firebase, user } = useContext(FirebaseContext);
   const { handleSubmit, handleChange, values, errors } = useFormValidation(
     INITIAL_STATE,
     validateCreateLink,
@@ -15,7 +17,24 @@ function CreateLink(props: any) {
   );
 
   function handleCreateLink() {
-    console.log("link created!");
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      const { url, description } = values;
+      const newLink = {
+        url,
+        description,
+        postedBy: {
+          id: user.uid,
+          name: user.displayName
+        },
+        votes: [],
+        comments: [],
+        created: Date.now()
+      };
+      firebase.db.collection("links").add(newLink);
+      props.history.push("/");
+    }
   }
 
   return (
