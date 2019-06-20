74a21a4737a9bf77b6c81b28f05c11416fbc6dc3
Kiichi Tachibana <>
2019-06-20 20:56:13 +0900

    Add effect to fetch links

--- src/components/Link/LinkList.tsx
+++ src/components/Link/LinkList.tsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { FirebaseContext } from "../../firebase";
 
 function LinkList(props: any) {
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    getLinks();
+  }, []);
+
+  function getLinks() {
+    firebase.db.collection("links").onSnapshot(handleSnapshot);
+  }
+
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map(doc => {
+      return { id: doc.id, ...doc.data() };
+    });
+    console.log({ links });
+  }
+
   return <div>LinkList</div>;
 }
 
