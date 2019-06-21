33edf6093121f5f41fde49900339cff9039619a4
ossan-engineer <bodega1123@gmail.com>
2019-06-21 10:22:42 +0900

    Implement voting

--- src/components/Link/LinkItem.tsx
+++ src/components/Link/LinkItem.tsx
@@ -1,28 +1,54 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { getDomain } from "../../utils";
-import distanceInWordsToNow from "date-fns/distance_in_words_to_now";
+import React, { useContext } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { getDomain } from '../../utils';
+import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
+import FirebaseContext from '../../firebase/context';
 
-function LinkItem({ showCount, link, index }: any) {
+function LinkItem({ showCount, link, index, history }: any) {
+  const { firebase, user } = useContext(FirebaseContext);
+
+  function handleVote() {
+    if (!user) {
+      history.push('/login');
+    } else {
+      const voteRef = firebase.db.collection('links').doc(link.id);
+      voteRef.get().then(doc => {
+        if (doc.exists) {
+          const previousVotes = doc.data().votes;
+          const vote = {
+            votedBy: {
+              id: user.uid,
+              name: user.displayName
+            }
+          };
+          const updatedVotes = [...previousVotes, vote];
+          voteRef.update({ votes: updatedVotes });
+        }
+      });
+    }
+  }
+
   return (
     <div className="flex items-start mt2">
       <div className="flex items-center">
         {showCount && <span className="gray">{index}.</span>}
-        <div className="vote-button">▲</div>
+        <div onClick={handleVote} className="vote-button">
+          ▲
+        </div>
       </div>
       <div className="ml1">
         <div>
-          {link.description}{" "}
+          {link.description}{' '}
           <span className="link">({getDomain(link.url)})</span>
         </div>
         <div className="f6 lh-copy gray">
-          {link.votes.length} votes by {link.postedBy.name}{" "}
+          {link.votes.length} votes by {link.postedBy.name}{' '}
           {distanceInWordsToNow(link.created)}
-          {" | "}
+          {' | '}
           <Link to={`/link/${link.id}`}>
             {link.comments.length > 0
               ? `${link.comments.length} comments`
-              : "discuss"}
+              : 'discuss'}
           </Link>
         </div>
       </div>
@@ -30,4 +56,4 @@ function LinkItem({ showCount, link, index }: any) {
   );
 }
 
-export default LinkItem;
+export default withRouter(LinkItem);
