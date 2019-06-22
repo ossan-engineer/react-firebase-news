329c29f47e6c7cf8325291f689452ad2c9337372
Kiichi Tachibana <>
2019-06-22 18:40:30 +0900

    Implement pagination

--- src/components/Link/LinkList.tsx
+++ src/components/Link/LinkList.tsx
@@ -1,36 +1,79 @@
 import React, { useContext, useEffect, useState } from "react";
 import { FirebaseContext } from "../../firebase";
 import LinkItem from "./LinkItem";
+import { LINKS_PER_PAGE } from "../../utils";
 
 function LinkList(props: any) {
   const { firebase } = useContext(FirebaseContext);
   const [links, setLinks] = useState([]);
+  const [linkLength, setLinkLength] = useState(0);
+  const [cursor, setCursor] = useState(null);
+  const isNewPage = props.location.pathname.includes("new");
   const isTopPage = props.location.pathname.includes("top");
+  const page = Number(props.match.params.page);
+
+  console.log(links.length);
 
   useEffect(() => {
     const unsubscribe = getLinks();
     return () => unsubscribe();
-  }, [isTopPage]);
+  }, [isTopPage, page]);
 
   function getLinks() {
+    const hasCursor = !!cursor;
+
+    // firebase.db
+    //   .collection("links")
+    //   .get()
+    //   .then(snapshot => setLinkLength(snapshot.size));
+
     if (isTopPage) {
       return firebase.db
         .collection("links")
         .orderBy("voteCount", "desc")
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    } else if (page === 1) {
+      return firebase.db
+        .collection("links")
+        .orderBy("created", "desc")
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    } else if (hasCursor) {
+      return firebase.db
+        .collection("links")
+        .orderBy("created", "desc")
+        .startAfter(cursor.created)
+        .limit(LINKS_PER_PAGE)
         .onSnapshot(handleSnapshot);
     }
-    return firebase.db
-      .collection("links")
-      .orderBy("created", "desc")
-      .onSnapshot(handleSnapshot);
   }
 
   function handleSnapshot(snapshot) {
     const links = snapshot.docs.map(doc => {
       return { id: doc.id, ...doc.data() };
     });
+    const lastLink = links[links.length - 1];
     setLinks(links);
+    setCursor(lastLink);
   }
+
+  function visitPrevioustPage() {
+    if (page > 1) {
+      props.history.push(`/new/${page - 1}`);
+    }
+  }
+
+  function visitNextPage() {
+    console.log(page);
+    console.log(links && links.length);
+    console.log(LINKS_PER_PAGE);
+    if (page <= links.length / LINKS_PER_PAGE) {
+      props.history.push(`/new/${page + 1}`);
+    }
+  }
+
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 1;
 
   return (
     <div>
@@ -39,9 +82,19 @@ function LinkList(props: any) {
           key={link.id}
           showCount={true}
           link={link}
-          index={index + 1}
+          index={index + pageIndex}
         />
       ))}
+      {isNewPage && (
+        <div className="pagination">
+          <div className="pointer mr2" onClick={visitPrevioustPage}>
+            Previoust
+          </div>
+          <div className="pointer" onClick={visitNextPage}>
+            Next
+          </div>
+        </div>
+      )}
     </div>
   );
 }
