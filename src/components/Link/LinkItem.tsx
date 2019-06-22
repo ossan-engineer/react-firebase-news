979005ffe070997bffd8caf297e5b7b954a1209d
Kiichi Tachibana <>
2019-06-22 15:16:12 +0900

    Implement deleting links

--- src/components/Link/LinkItem.tsx
+++ src/components/Link/LinkItem.tsx
@@ -1,17 +1,29 @@
-import React, { useContext } from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import { getDomain } from '../../utils';
-import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
-import FirebaseContext from '../../firebase/context';
+import React, { useContext } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { getDomain } from "../../utils";
+import distanceInWordsToNow from "date-fns/distance_in_words_to_now";
+import FirebaseContext from "../../firebase/context";
 
 function LinkItem({ showCount, link, index, history }: any) {
   const { firebase, user } = useContext(FirebaseContext);
 
+  function handleDeleteLink() {
+    const linkRef = firebase.db.collection("links").doc(link.id);
+    linkRef
+      .delete()
+      .then(() => {
+        console.log(`Document with ID ${link.id} deleted`);
+      })
+      .catch(err => {
+        console.error("Error deleting document: ", err);
+      });
+  }
+
   function handleVote() {
     if (!user) {
-      history.push('/login');
+      history.push("/login");
     } else {
-      const voteRef = firebase.db.collection('links').doc(link.id);
+      const voteRef = firebase.db.collection("links").doc(link.id);
       voteRef.get().then(doc => {
         if (doc.exists) {
           const previousVotes = doc.data().votes;
@@ -28,6 +40,8 @@ function LinkItem({ showCount, link, index, history }: any) {
     }
   }
 
+  const postedByAuthUser = user && user.uid === link.postedBy.id;
+
   return (
     <div className="flex items-start mt2">
       <div className="flex items-center">
@@ -38,18 +52,26 @@ function LinkItem({ showCount, link, index, history }: any) {
       </div>
       <div className="ml1">
         <div>
-          {link.description}{' '}
+          {link.description}{" "}
           <span className="link">({getDomain(link.url)})</span>
         </div>
         <div className="f6 lh-copy gray">
-          {link.votes.length} votes by {link.postedBy.name}{' '}
+          {link.votes.length} votes by {link.postedBy.name}{" "}
           {distanceInWordsToNow(link.created)}
-          {' | '}
+          {" | "}
           <Link to={`/link/${link.id}`}>
             {link.comments.length > 0
               ? `${link.comments.length} comments`
-              : 'discuss'}
+              : "discuss"}
           </Link>
+          {postedByAuthUser && (
+            <>
+              {" |  "}
+              <span className="delete-button" onClick={handleDeleteLink}>
+                delete
+              </span>
+            </>
+          )}
         </div>
       </div>
     </div>
